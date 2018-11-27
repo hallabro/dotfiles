583820267f6d37a9024d0ea0e6984caf79f3c8e4
Robin Hallabro <robin@hallabro.nu>
2018-11-27 20:53:51 +0100

    emacs: Update configuration

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -20,71 +20,89 @@
 (setq default-fill-column 80)
 (setq byte-compile-warnings nil)
 (global-display-line-numbers-mode)
+(global-set-key (kbd "<escape>") 'keyboard-escape-quit)
 (setq-default mode-line-format nil)
 (menu-bar-mode -1) 
 
-(use-package evil
-  :ensure t
+(defun switch-to-last-buffer ()
+  (interactive)
+  (switch-to-buffer (other-buffer (current-buffer) 1)))
+
+(when window-system
+  (scroll-bar-mode -1)
+  (tool-bar-mode -1))
+
+(use-package evil :ensure t
   :config
   (evil-mode t))
 
-(use-package evil-easymotion
-  :ensure t
-  )
-
-(use-package helm
-  :ensure t
+(use-package helm :ensure t
   :config
   (require 'helm-config)
   (helm-mode 1)
-  (define-key helm-map (kbd "C-j") 'helm-next-line)
-  (define-key helm-map (kbd "C-k") 'helm-previous-line)
+  (global-set-key (kbd "M-x") 'helm-M-x)
+  :bind (:map helm-map
+	      ("C-j" . 'helm-next-line)
+	      ("C-k" . 'helm-previous-line))
   )
 
-(use-package ido
-  :config
-  (ido-mode 1)
-  )
-
-(use-package base16-theme
-  :ensure t
+(use-package base16-theme :ensure t
   :config
   (setq base16-theme-256-color-source "colors")
   (load-theme 'base16-default-dark t)
   )
 
-(use-package hydra
-  :ensure t
-  :config
-  (defhydra hydra-buffers (:hint nil)
-    "
-^Switch^
-^^^^^^^^-----------------------------------------------------------------
-_l_: list
-"
-    ("l" helm-buffers-list :exit t)
-    )
-  (defhydra hydra-menu nil
-    "menu"
-    ("b" hydra-buffers/body "buffer" :exit t)
-    )
-  (define-key evil-normal-state-map (kbd "SPC") 'hydra-menu/body))
+(use-package hydra :ensure t :defer t
+  :bind (:map evil-normal-state-map
+	      ("SPC" . hydra-menu/body)))
 
-
-(use-package evil-snipe
-  :ensure t
-  :config
-  (evil-snipe-mode 1)
-  (setq evil-snipe-scope 'whole-buffer)
-  (setq evil-snipe-repeat-scope 'whole-buffer)
-  (setq evil-snipe-show-prompt nil)
+(defhydra hydra-buffers (:color blue)
+  ("l" helm-mini "list" :column "switch")
+  ("s" helm-do-ag-buffers "search in buffers")
+  ("a" save-buffer "save" :column "action")
+  ("k" (kill-buffer (current-buffer)) "kill")
   )
 
+(defhydra hydra-emacs (:color blue)
+  ("r" (load-file "~/.emacs.d/init.el") "reload")
+  ("q"  save-buffers-kill-terminal "save and quit")
+  )
 
-(use-package linum-relative
-  :ensure t
+(defhydra hydra-projects (:color blue)
+  ("w" helm-projectile-switch-project "switch")
+  ("s" helm-do-ag-project-root "search")
+  ("d" projectile-discover-projects-in-directory "discover"))
+
+(defhydra hydra-menu (:color blue)
+  ("b" hydra-buffers/body "buffer" :exit t)
+  ("e" hydra-emacs/body "emacs" :exit t)
+  ("p" hydra-projects/body "projects" :exit t))
+
+(use-package avy :ensure t
+  :bind (:map evil-normal-state-map
+	      ("s" . avy-goto-char-2)))
+
+(use-package dtrt-indent :ensure t
+  :config
+  (dtrt-indent-mode 1))
+
+(use-package linum-relative :ensure t
   :config
   (setq linum-relative-backend 'display-line-numbers-mode)
   (linum-on)
   (linum-relative-mode)
-  )
+  (helm-linum-relative-mode 1))
+
+(use-package projectile :ensure t
+  :config
+  (projectile-mode 1))
+
+(use-package helm-projectile :ensure t)
+(use-package helm-ag :ensure t)
+
+(use-package key-chord
+  :ensure t
+  :config
+  (key-chord-mode 1)
+  (key-chord-define evil-insert-state-map  "jk" 'evil-normal-state)
+  (key-chord-define-global "JJ" 'switch-to-last-buffer))
