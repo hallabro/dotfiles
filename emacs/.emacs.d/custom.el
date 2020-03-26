bd2b9e6906f019f8ea0b32a3ed90d5bb0a0ab841
Robin Hallabro-Kokko <robin@hallabro.nu>
2020-03-26 23:18:39 +0100

    emacs: cosmetic changes

--- emacs/.emacs.d/custom.el
+++ emacs/.emacs.d/custom.el
@@ -3,9 +3,13 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("e1498b2416922aa561076edc5c9b0ad7b34d8ff849f335c13364c8f4276904f0" default)))
  '(package-selected-packages
    (quote
-    (org-mode python-mode php-mode evil-surround markdown-mode markdown ace-window super-save key-chord key-cord helm-ag helm-projectile projectile dtrt-indent dtrt-indent-mode major-mode-hydra linum-relative base16-theme hydra helm-descbinds hel helm helm-config evil-easymotion evil-snipe use-package evil))))
+    (org-mode python-mode php-mode evil-surround markdown-mode markdown ace-window super-save key-chord key-cord helm-ag helm-projectile projectile dtrt-indent dtrt-indent-mode major-mode-hydra linum-relative base16-theme hydra helm-descbinds hel helm helm-config evil-easymotion evil-snipe use-package evil)))
+ '(projectile-git-command "git ls-files -zco --exclude-standard -- . ':!:vendor/*'"))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
